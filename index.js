09fb4d4c6d608ba5642c35e37bc969921fbbe9f1
yehudisr <77118499+yehudisr@users.noreply.github.com>
2021-03-15 09:28:11 -0400

    commit tree 9a06c0fd6773c5b998378c900738b7cebe9cd0f4

--- index.js
+++ index.js
@@ -1,1 +1,17 @@
-// Code your solutions in this file
+function writeCards( namesArray, event ) {
+    let thankYouCards = []
+    for ( let i = 0; i < namesArray.length; i++ ) {
+      thankYouCards.push( `Thank you, ${namesArray[i]}, for the wonderful ${event} gift!` )
+    }
+    return thankYouCards
+  }
+  
+
+function countDown(i){
+  
+    while(i > 0){
+        console.log(i);
+        i--;
+    }
+    console.log(i);
+}
